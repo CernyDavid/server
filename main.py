833b545b060aa737e04a5ade906927af03276344
David Černý <91247706+CernyDavid@users.noreply.github.com>
2022-04-28 11:36:08 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,10 @@
+radio.set_group(80)
+hlasy = [{"sernumb": 1254, "hlas": "A"}, {"sernumb": 8744, "hlas": "B"}]
+
+def on_received_value(hlas, cislo):
+    pass
+radio.on_received_value(on_received_value)
+def on_received_value1(serialnumber, sernumb):
+    for x in hlasy:
+        pass
+radio.on_received_value(on_received_value1)
---
+++ main.ts
@@ -0,0 +1,18 @@
+radio.setGroup(80)
+let hlasy = [ {
+    "sernumb" : 1254,
+    "hlas" : "A",
+}
+, {
+    "sernumb" : 8744,
+    "hlas" : "B",
+}
+]
+radio.onReceivedValue(function on_received_value(hlas: string, cislo: number) {
+    
+})
+radio.onReceivedValue(function on_received_value1(serialnumber: string, sernumb: number) {
+    for (let x of hlasy) {
+        
+    }
+})
