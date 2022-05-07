7655eea6f2d8e3c6f201a538771e4c4b3375ca9e
David Černý <91247706+CernyDavid@users.noreply.github.com>
2022-05-07 12:49:40 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,11 +1,47 @@
 radio.set_group(80)
-h: any = []
-hlasy = [{"sernumb": 1254, "hlas": "A"}, {"sernumb": 8744, "hlas": "B"}]
+povoleno = 0
+counter = 0
+number = 0
+
+def on_button_pressed_b():
+    global povoleno
+    if povoleno == 0:
+        povoleno = 1
+        basic.show_icon(IconNames.YES)
+    elif povoleno == 1:
+        povoleno = 0
+        basic.show_icon(IconNames.NO)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_button_pressed_a():
+    radio.send_value("povoleni", povoleno)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    global hlasy
+    hlasy.clear()
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+hlasy: any = [{"sernumb": 0, "hlas": "0"}]
+vote = {"sernumb": 1254, "hlas": "A"}
 
 def on_received_value(hlas, cislo):
-    pass
+    number = cislo
+    vote["hlas"] = String.from_char_code(number+65)
 radio.on_received_value(on_received_value)
 def on_received_value1(serialnumber, sernumb):
+    global counter
     for x in hlasy:
-        pass
+        if hlasy[counter]["sernumb"] == sernumb:
+            hlasy[counter]["hlas"] = String.from_char_code(number+65)
+            counter = 0
+            break
+        counter += 1
+    if counter == len(hlasy):
+        vote["sernumb"] = sernumb        
+        hlasy.append(vote)
+    serial_confirm = sernumb
+    radio.send_value("confirm", serial_confirm)
 radio.on_received_value(on_received_value1)
+
+console.log_value("hlasy", hlasy)
--- main.ts
+++ main.ts
@@ -1,19 +1,56 @@
 radio.setGroup(80)
-let h : any[] = []
+let povoleno = 0
+let counter = 0
+let number = 0
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    if (povoleno == 0) {
+        povoleno = 1
+        basic.showIcon(IconNames.Yes)
+    } else if (povoleno == 1) {
+        povoleno = 0
+        basic.showIcon(IconNames.No)
+    }
+    
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    radio.sendValue("povoleni", povoleno)
+})
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
+    _py.py_array_clear(hlasy)
+})
 let hlasy = [ {
+    "sernumb" : 0,
+    "hlas" : "0",
+}
+]
+let vote = {
     "sernumb" : 1254,
     "hlas" : "A",
 }
-, {
-    "sernumb" : 8744,
-    "hlas" : "B",
-}
-]
+
 radio.onReceivedValue(function on_received_value(hlas: string, cislo: number) {
-    
+    let number = cislo
+    vote["hlas"] = String.fromCharCode(number + 65)
 })
 radio.onReceivedValue(function on_received_value1(serialnumber: string, sernumb: number) {
+    
     for (let x of hlasy) {
+        if (hlasy[counter]["sernumb"] == sernumb) {
+            hlasy[counter]["hlas"] = String.fromCharCode(number + 65)
+            counter = 0
+            break
+        }
         
+        counter += 1
     }
+    if (counter == hlasy.length) {
+        vote["sernumb"] = sernumb
+        hlasy.push(vote)
+    }
+    
+    let serial_confirm = sernumb
+    radio.sendValue("confirm", serial_confirm)
 })
+console.logValue("hlasy", hlasy)
